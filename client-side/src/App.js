d149a2ed3651dc1485534a145522364c5114bfe1
dvora <dvoras2000@gmail.com>
2024-07-07 23:45:09 +0300

    add timer

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,9 +1,10 @@
 
 import './App.css';
+import StopWatch from './timer/stopWatch'
 function App() {
   return (
     <div className="App">
-        
+        <StopWatch></StopWatch>
     </div>
   );
 }
