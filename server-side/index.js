f258f9e44ac10ace7682bfbe222fa739d5f75a48
moriya2004 <mor2004@gmail.com>
2024-07-08 14:12:25 +0300

    update server-side

--- server-side/index.js
+++ server-side/index.js
@@ -23,18 +23,7 @@ app.get('/', (req, res) => {
 })
 
 
-// Middleware to authenticate JWT token
-// const authenticateToken = (req, res, next) => {
-//     const authHeader = req.headers['authorization'];
-//     const token = authHeader && authHeader.split(' ')[1];
-//     if (token == null) return res.sendStatus(401); // Unauthorized
 
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-//         if (err) return res.sendStatus(403); // Forbidden
-//         req.user = user;
-//         next();
-//     });
-// };
 
 
 
--- server-side/models/profile.js
+++ server-side/models/profile.js
@@ -1,18 +1,44 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
+
+
 const profileSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    profileName: { type: String, required: true },
+    profileName: { type: String, required: true, minlength: 2, maxlength: 50 },
     blockedSites: [{ type: Schema.Types.ObjectId, ref: 'Website' }],
     limitedWebsites: [{
         websiteId: { type: Schema.Types.ObjectId, ref: 'Website' },
         status: { type: String, enum: ['block', 'open'] },
-        limitedTimes: {
-            start: { type: Number },
-            end: { type: Number }
-        }
+        limitedTimes: [{
+            start: {
+                hours: { type: Number, min: 0, max: 23 },
+                minutes: { type: Number, min: 0, max: 59 },
+                validate: {
+                    validator: function() {
+                        // בודק אם השעה והדקה תקינים
+                        return Number.isInteger(this.hours) && Number.isInteger(this.minutes);
+                    },
+                    message: 'Invalid start time'
+                }
+            },
+            end: {
+                hours: { type: Number, min: 0, max: 23 },
+                minutes: { type: Number, min: 0, max: 59 },
+                validate: {
+                    validator: function() {
+                        // בודק אם השעה והדקה תקינים וגדולים מה-start
+                        return Number.isInteger(this.hours) && Number.isInteger(this.minutes) && 
+                            (this.hours > this.start.hours || (this.hours === this.start.hours && this.minutes > this.start.minutes));
+                    },
+                    message: 'End time must be greater than start time'
+                }
+            }
+        }]
     }]
 });
+
+
+
 
 export default mongoose.model("Profile", profileSchema);
