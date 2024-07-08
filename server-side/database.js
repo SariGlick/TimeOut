d5df8506f5db5f9596d475ab52e85b52f0139823
moriya2004 <mor2004@gmail.com>
2024-07-08 10:39:44 +0300

    Add server-side directory with initial files

--- server-side/database.js
+++ server-side/database.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 
-// Replace the uri string with your connection string.
 
-//const uriLocal = "mongodb://localhost:27017/TimeOut";
 
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGODB_URI);
