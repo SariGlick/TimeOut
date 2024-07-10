424666964d59814314061f9e34bc989606be98be
dvora <dvoras2000@gmail.com>
2024-07-10 08:59:08 +0300

    add dependencies

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import StackedAreas from './graphs/graphs.jsx'
 function App() {
   return (
     <div className="App">
         
-      
+      <StackedAreas></StackedAreas>
     </div>
   );
 }
