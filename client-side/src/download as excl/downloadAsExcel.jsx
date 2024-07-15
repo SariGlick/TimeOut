4cc67ffeb85c227b721a637ba0008b6aff54eaa7
lielsa2023 <lielsa1160@gmail.com>
2024-07-15 16:02:11 +0300

    after comments

--- client-side/src/download as excl/downloadAsExcel.jsx
+++ client-side/src/download as excl/downloadAsExcel.jsx
@@ -1,16 +1,27 @@
-
-
+import React from 'react';
 import { saveAs } from 'file-saver';
 import * as XLSX from 'xlsx';
+import GenericButton from '../stories/Button/GenericButton';
 
-export const downloadExcel = (data) => {  
+
+const downloadExcel = (data, sheetName = 'Browsing Data', fileName = 'browsing_data.xlsx') => {  
   const workbook = XLSX.utils.book_new();  
   const worksheet = XLSX.utils.json_to_sheet(data);  
-  XLSX.utils.book_append_sheet(workbook, worksheet, 'Data');  
+  XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);  
   const excelBuffer = XLSX.write(workbook, {
     bookType: 'xlsx',
     type: 'array'
   });  
   const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });  
-  saveAs(blob, 'data.xlsx');
+  saveAs(blob, fileName);
 };
+
+const DownloadAsExcel = ({ data, sheetName, fileName }) => {
+  return (
+    <div>
+      <GenericButton onClick={() => downloadExcel(data, sheetName, fileName)}>Download as Excel</GenericButton>
+    </div>
+  );
+};
+
+export default DownloadAsExcel;
