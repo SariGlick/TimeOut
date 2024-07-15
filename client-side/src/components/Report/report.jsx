281aa72edfbc9bf11e62e8bda794a8770618920c
ayeletBinder <ayeletbinder@gmail.com>
2024-07-15 09:53:23 +0300

    change stylies to variables

--- client-side/src/components/Report/report.jsx
+++ client-side/src/components/Report/report.jsx
@@ -21,17 +21,10 @@ export default function Report() {
     else   if(selectedValue==4){
       setCustum(true);
     }
-
   };
   return (
     <div>
-      {/* לשים היכונים- לשאול את חני איך היא התכוונה לשים אותם - כי הם לא בתוך תגית של תמונה. */}
-      {/* הפונקציה של הסלקט איך היא מקבלת מי עבר עליה לא אמור ליהיות event.target? */}
         <Select onChange={(selectedValue) => selectFunctions(selectedValue) } className="primary" options={[{value:1,text:"day",iconSrc:'/images/day.png'},{value:2,text:"month",iconSrc:'images/month.png'},{value:3,text:"year",iconSrc:'/images/year.png'},{value:4,text:"custum",iconSrc:'/images/custum.png'}]}  title="time arrange" size='medium' widthOfSelect="150px"/>
-        {/* {custum&&}
-        {year&&}  
-        {day&&}
-        {mounth&&} */}
         <PdfGenerator data={[{SiteName:'gmail',BrowsingTime:20,AvgForADay:1}]} />
     </div>
   );
