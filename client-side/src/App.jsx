3bb01e67956254aef953263d74b97ec142eace67
ayeletBinder <ayeletbinder@gmail.com>
2024-07-15 11:29:42 +0300

    fix conflict

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.scss';
 import Header from './stories/header/header.jsx';
 import Report from './components/Report/report.jsx';
+import downloadPDF from './components/Report/diagram.jsx';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Report/>
+      {/* <Report/> */}
+      <downloadPDF/>
       <img src="/public/month.png" alt="" />
     </div>
   );
