0cd427122c71e14a735809fd471d783c8f253695
efratronit <efratha04@gmail.com>
2024-07-16 12:00:48 +0300

    update_node

--- server-side/models/user.model.js
+++ server-side/models/user.model.js
@@ -1,19 +1,23 @@
 import mongoose from 'mongoose';
-
+import jwt from 'jsonwebtoken';
 const { Schema } = mongoose;
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String },
   googleId: { type: String },
-<<<<<<< HEAD
-  visitsWebsites: [{ type: Schema.Types.ObjectId, ref: 'Websites' }],
-=======
+
   visitsWebsites: [{ type: Schema.Types.ObjectId, ref: 'VisitedWebsites' }],
->>>>>>> moriya/server-side
+
   profiles: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
   preferences: [{ type: Schema.Types.ObjectId, ref: 'Preference' }],
   profileImage: { type: String }
 });
 
 export default mongoose.model('Users', userSchema);
+export const generateToken = (user) => {
+  const privateKey = process.env.JWT_SECRET || 'JWT_SECRET';
+  const data = {  user_id: user._id };
+  const token = jwt.sign(data, privateKey, { expiresIn: '1h' });
+  return token;
+};
