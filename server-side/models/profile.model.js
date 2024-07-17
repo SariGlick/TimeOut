9c9596b02b4e1a2a104856f9196979d335dd428c
MiryamGabay <miryamgabay9@gmail.com>
2024-07-17 12:06:04 +0300

    add service and changes profile model

--- server-side/models/profile.model.js
+++ server-side/models/profile.model.js
@@ -1,23 +1,35 @@
 import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
 
-
+const { Schema } = mongoose;
 
 const profileSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     profileName: { type: String, required: true, minlength: 2, maxlength: 50 },
-    blockedSites: [{ type: Schema.Types.ObjectId, ref: 'Websites' }],
-    limitedWebsites: [{
+    statusBlockedSites: { type: String, enum: ['black list', 'white list'], required: true },
+    listWebsites: [{
         websiteId: { type: Schema.Types.ObjectId, ref: 'Websites' },
-        status: { type: String, enum: ['block', 'open'] },
-        limitedTimes: [{
-            start: {type:Date, default:new Date()},
-            end: {type:Date, default:new Date()}
-        }]
+        status: { type: String, enum: ['block', 'open', 'limit'] },
+        limitedMinutes: { type: Number, default: () => new Date().getHours(), required: true },
+    }],
+    profileTime: [{
+        start: { type: Date, default: new Date() },
+        end: { type: Date, default: new Date() }
     }]
 });
 
+// Middleware to check the status of listWebsites before saving
+profileSchema.pre('save', function (next) {
+    const profile = this;
 
+    if (profile.statusBlockedSites === 'white list') {
+        profile.listWebsites.forEach(site => {
+            if (site.status === 'block') {
+                return next(new Error('In white list mode, site status cannot be "block"'));
+            }
+        });
+    }
+    
+    next();
+});
 
-
-export  default mongoose.model("Profiles", profileSchema);
+export default mongoose.model("Profiles", profileSchema);
