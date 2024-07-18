1285fedef76bc4852cf88814311f189f7634bcc6
MiryamGabay <miryamgabay9@gmail.com>
2024-07-18 16:22:35 +0300

    addTimer

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,13 +1,21 @@
 import { Route, Routes } from 'react-router';
 import LabTabs from './stories/tabs/tabs';
-import Header from './stories/header/header'
-import Footer from './stories/footer/FooterComponent'
+import Header from './stories/header/header';
+import Footer from './stories/footer/FooterComponent';
+import ProfileActivationTimer from './stories/ProfileActivationTimer'
+
 import './App.scss';
 
 function App() {
+
+  const profileActivationTime = 1;
+
   return (
     <div className="App">
       <Header/> 
+
+      <ProfileActivationTimer profileActivationTime={profileActivationTime} />
+
       <Footer/>
      
 
