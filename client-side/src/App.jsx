4ac6859cfb7f6bae91b4009c26cb45a89da3f57b
moriya2004 <mor2004@gmail.com>
2024-07-19 00:39:45 +0300

    settings email freq

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from './stories/header/header'
 import Footer from './stories/footer/FooterComponent'
-import Settings from './components/settings/Settings';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Settings></Settings>
       <Footer />
     </div>
   );
