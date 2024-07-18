710a7df2a9bbd5733b07d7af38bf5aa8c63f2899
lielsa2023 <lielsa1160@gmail.com>
2024-07-18 13:41:46 +0300

    Committing changes before revert

--- client-side/src/stories/genericInput.jsx
+++ client-side/src/stories/genericInput.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
-import '../style/genericInput.scss';
+import  '../style/genericInput.scss';
 
-const GenericInput = ({ label, type='text', value, onChange, size='medium', width='20%',
-   icon: Icon, validation, ...rest }) => {
+const GenericInput = ({ label, type='text', value, onChange, size='medium',
+   icon: Icon,validation, ...rest }) => {
   const handleChange = (e) => {
     if (onChange) {
       onChange(e.target.value);
     }
   };
+
 
   let error = false;
   let helperText = '';
@@ -23,12 +24,8 @@ const GenericInput = ({ label, type='text', value, onChange, size='medium', widt
     }
   }
 
-  const inputStyle = {
-    width: width
-  };
-
   return (
-    <div className="generic-input">
+    <div className={`generic-input `} >
       <TextField
         label={label}
         type={type}
@@ -36,7 +33,7 @@ const GenericInput = ({ label, type='text', value, onChange, size='medium', widt
         onChange={handleChange}
         size={size}
         error={error}
-        helperText={helperText} 
+        helperText={helperText}       
         InputProps={{
           startAdornment: Icon && (
             <InputAdornment position="start">
@@ -45,7 +42,6 @@ const GenericInput = ({ label, type='text', value, onChange, size='medium', widt
           ),
           ...rest.InputProps,
         }}
-        style={inputStyle}
         {...rest}
       />
     </div>
@@ -58,9 +54,10 @@ GenericInput.propTypes = {
   value: PropTypes.string,
   onChange: PropTypes.func,
   size: PropTypes.oneOf(['small', 'medium']),
-  width: PropTypes.string, 
+  width:PropTypes.oneOf(['small','medium']),
   icon: PropTypes.elementType,
   validation: PropTypes.func,
+ 
 };
 
 export default GenericInput;
