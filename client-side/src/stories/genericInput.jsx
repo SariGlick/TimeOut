66acb144ba91c4821a5b1efb5248d69d9e331f17
lielsa2023 <lielsa1160@gmail.com>
2024-07-18 14:43:09 +0300

    delete 2 files

--- client-side/src/stories/genericInput.jsx
+++ client-side/src/stories/genericInput.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
-import  '../style/genericInput.scss';
 
-const GenericInput = ({ label, type='text', value, onChange, size='medium',
-   icon: Icon,validation, ...rest }) => {
+import '../style/genericInput.scss';
+
+const GenericInput = ({ label, type='text', value, onChange, size='medium', width='20%',
+   icon: Icon, validation, ...rest }) => {
+
   const handleChange = (e) => {
     if (onChange) {
       onChange(e.target.value);
     }
   };
+
+
+
 
 
   let error = false;
@@ -24,6 +29,7 @@ const GenericInput = ({ label, type='text', value, onChange, size='medium',
     }
   }
 
+
   return (
     <div className={`generic-input `} >
       <TextField
@@ -33,7 +39,9 @@ const GenericInput = ({ label, type='text', value, onChange, size='medium',
         onChange={handleChange}
         size={size}
         error={error}
-        helperText={helperText}       
+ 
+        helperText={helperText} 
+
         InputProps={{
           startAdornment: Icon && (
             <InputAdornment position="start">
@@ -54,7 +62,11 @@ GenericInput.propTypes = {
   value: PropTypes.string,
   onChange: PropTypes.func,
   size: PropTypes.oneOf(['small', 'medium']),
-  width:PropTypes.oneOf(['small','medium']),
+
+ 
+
+  width: PropTypes.string, 
+
   icon: PropTypes.elementType,
   validation: PropTypes.func,
  
