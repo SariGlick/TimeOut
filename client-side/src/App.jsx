1731f4a28facc020d976ee5890fe40a1b9de0f6d
Batsheva <batshevatavger@gmail.com>
2024-07-21 11:16:44 +0300

    add profile Image

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './stories/header/header'
 import Footer from './stories/footer/FooterComponent'
 import './App.scss';
+import ProfileImageEditButton from './components/settings/ProfileImageEdit';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProfileImageEditButton userId={'6694d2295d41f7809588274c'}/>
       <Footer />
     </div>
   );
