c28d1d81fd209fc8dea0ee6f379af21191a532a6
ayeletBinder <ayeletbinder@gmail.com>
2024-07-22 09:55:51 +0300

    fix the html

---
+++ client-side/src/components/Report/report.constant.jsx
@@ -0,0 +1,13 @@
+export const Time = {
+    DAY:{name:"day",icon:'/images/time-twenty-four.svg'},
+    Month:{name:"month",icon:'images/calendar-date-svgrepo-com.svg'},
+    YEAR:{name:"year",icon:'/images/calendar-month-schedule-time-date-svgrepo-com.svg'},
+    CUSTUM:{name:"custum",icon:'/images/calendar-date-svgrepo-com (1).svg'}
+  };
+
+  export const table = [
+    { title: "Site name", dataKey: "SiteName" },
+    { title: "Browsing time", dataKey: "BrowsingTime" },
+    { title: "Avg. for a day", dataKey: "AvgForADay" },
+  ];
+
