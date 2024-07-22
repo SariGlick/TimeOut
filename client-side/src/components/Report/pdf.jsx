d33fcf866933cec320e7e8e11713b8a05353cbf6
ayeletBinder <ayeletbinder@gmail.com>
2024-07-22 10:41:03 +0300

    fix the html the import

--- client-side/src/components/Report/pdf.jsx
+++ client-side/src/components/Report/pdf.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import jsPDF from 'jspdf';
-import 'jspdf-autotable';
+import autoTable from 'jspdf-autotable';
 import GenericButton from '../../stories/Button/GenericButton';
 import { table } from './report.constant';
 
-const PdfGenerator = ({data,nameFile="timeOut-Report",columnsforthetable=table,textLable="download pdf"}) => {
-const userName="name"; //change to realy name
+const PdfGenerator = ({ data, nameFile = "timeOut-Report", columnsforthetable = table, textLable = "download psectionRows" }) => {
+  const userName = "name"; 
+
   const generatePdf = () => {
     const doc = new jsPDF();
-    const date=new Date();
-    doc.text(["Report:",`issue Date: ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,`userName: ${userName}`], 10, 10);
-    const columns = columnsforthetable;
+    const date = new Date();
+    
+    doc.text([
+      "Report:", 
+      `issue Date: ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
+      `userName: ${userName}`
+    ], 10, 10);
+    
     doc.autoTable({
-      columns: columns,
+      columns: columnsforthetable,
       body: data,
       startY: 35,
     });
+
     doc.text("developed by Extratech", 10, 250);
+
     doc.save(`${nameFile}.pdf`);
   };
- 
+
   return (
     <div>
-      <GenericButton className="secondary" label={textLable} size="medium" onClick={generatePdf}></GenericButton>
+      <GenericButton className="secondary" label={textLable} size="medium" onClick={generatePdf} />
     </div>
   );
 };
