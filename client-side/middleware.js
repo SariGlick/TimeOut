a780d6a1aca1007155d42aa59d098364ea904bbc
dvora <dvoras2000@gmail.com>
2024-07-22 15:39:36 +0300

    added changes in middleware.js

--- client-side/middleware.js
+++ client-side/middleware.js
@@ -7,14 +7,14 @@ export default async function get(path) {
 
 };
 
-export default async function post(path) {
-    const response = await axios.post(path);
+export default async function post(path, data) {
+    const response = await axios.post(path, data);
     return response;
 
 };
 
-export default async function put(path) {
-    const response = await axios.put(path);
+export default async function put(path, data) {
+    const response = await axios.put(path, data);
     return response;
 
 };
