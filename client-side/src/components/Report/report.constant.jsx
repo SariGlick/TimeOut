610156aa5840927f3cb43445586819887e52d966
ayeletBinder <ayeletbinder@gmail.com>
2024-07-23 12:08:08 +0300

    change option array

--- client-side/src/components/Report/report.constant.jsx
+++ client-side/src/components/Report/report.constant.jsx
@@ -1,8 +1,8 @@
 export const Time = {
-    DAY:{name:"day",icon:'/images/time-twenty-four.svg'},
-    Month:{name:"month",icon:'images/calendar-date-svgrepo-com.svg'},
-    YEAR:{name:"year",icon:'/images/calendar-month-schedule-time-date-svgrepo-com.svg'},
-    CUSTUM:{name:"custum",icon:'/images/calendar-date-svgrepo-com (1).svg'}
+    DAY:{value:1,text:"day",iconSrc:'/images/time-twenty-four.svg'},
+    Month:{value:2,text:"month",iconSrc:'images/calendar-date-svgrepo-com.svg'},
+    YEAR:{value:3,text:"year",iconSrc:'/images/calendar-month-schedule-time-date-svgrepo-com.svg'},
+    CUSTUM:{value:4,text:"custum",iconSrc:'/images/calendar-date-svgrepo-com (1).svg'}
   };
 
   export const table = [
@@ -10,4 +10,4 @@ export const Time = {
     { title: "Browsing time", dataKey: "BrowsingTime" },
     { title: "Avg. for a day", dataKey: "AvgForADay" },
   ];
-
+export const optionArray=[Time.DAY,Time.Month,Time.YEAR,Time.CUSTUM];
--- client-side/src/components/Report/report.jsx
+++ client-side/src/components/Report/report.jsx
@@ -1,22 +1,22 @@
 import React,{useState} from 'react';
-import PdfGenerator from './pdf.jsx';
+// import PdfGenerator from './pdf.jsx';
 import Select from '../../stories/Select/Select.jsx';
-import { Time } from './report.constant.jsx';
+import { Time, optionArray } from './report.constant.jsx';
 
 export default function Report() {
   const [selectType, setSelectType] = useState(Time.Month);
   const selectFunction=(selectedValue)=>{
     if(selectedValue==1){
-      setSelectType(Time.DAY.name);
+      setSelectType(Time.DAY.text);
     }
     else   if(selectedValue==2){
-      setSelectType(Time.Month.name);
+      setSelectType(Time.Month.text);
     }
     else   if(selectedValue==3){
-      setSelectType(Time.YEAR.name);
+      setSelectType(Time.YEAR.text);
     }
     else   if(selectedValue==4){
-      setSelectType(Time.CUSTUM.name);
+      setSelectType(Time.CUSTUM.text);
       dateTimePiker();
     }
     fillData();
@@ -29,8 +29,8 @@ export default function Report() {
   };
   return (
     <div>
-        <Select onChange={(selectedValue) => selectFunction(selectedValue) } className="primary" options={[{value:1,text:Time.DAY.name,iconSrc:Time.DAY.icon},{value:2,text:Time.Month.name,iconSrc:Time.Month.icon},{value:3,text:Time.YEAR.name,iconSrc:Time.YEAR.icon},{value:4,text:Time.CUSTUM.name,iconSrc:Time.CUSTUM.icon}]}  title="time arrange" size='medium' widthOfSelect="150px"/>
-        <PdfGenerator data={[]} />   {/* כאן נשלח את הנתונים של הטבלה כדי שיודפסו בקובץ פדפ */}
+        <Select onChange={(selectedValue) => selectFunction(selectedValue) } className="primary" options={optionArray}  title="time arrange" size='medium' widthOfSelect="150px"/>
+        {/* <PdfGenerator data={[]} />   כאן נשלח את הנתונים של הטבלה כדי שיודפסו בקובץ פדפ */}
     </div>
   );
 }
