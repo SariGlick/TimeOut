465be49692b46a9c6c718805979d51efec0dbb2c
aidy8847 <aw7658847@gmail.com>
2024-07-23 13:44:59 +0300

    change the index

--- client-side/src/stories/index.js
+++ client-side/src/stories/index.js
@@ -4,21 +4,21 @@ import Footer from './footer/FooterComponent';
 import GenericInput from './GenericInput/genericInput';
 import ResponsiveAppBar from './header/header';
 import Select from './Select/Select';
-//import ScrollButton from './ScrollButton/ScrollButton'
-// import TableComponent from './table/TableComponent';
-//import RadioButton from './RadioButton/radio-Button';
-//import ToastMessage from './Toast/ToastMessage'
+import ScrollButton from './ScrollButton/ScrollButton'
+import TableComponent from './table/TableComponent';
+import RadioButton from './RadioButton/radio-Button';
+import ToastMessage from './Toast/ToastMessage'
 import LabTabs from './tabs/tabs';
 
-export{
+export {
     GenericButton,
     DateInput,
     Footer,
     GenericInput,
     ResponsiveAppBar,
     Select,
-   // ScrollButton,
-    // TableComponent,
+    ScrollButton,
+    TableComponent,
     LabTabs,
     RadioButton,
     ToastMessage
