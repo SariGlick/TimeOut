b3bdf59da825e521ae6146cf2932e8c7f7f29671
Shira-Abrams <sh3000444@gmail.com>
2024-07-25 13:37:20 +0300

    finish change noteTime

--- client-side/src/components/settings/Settings.jsx
+++ client-side/src/components/settings/Settings.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import GenericButton  from '../../stories/Button/GenericButton.jsx';
 import { useTranslation } from 'react-i18next'
+import GenericButton  from '../../stories/Button/GenericButton.jsx';
 import GenericInput from '../../stories/GenericInput/genericInput.jsx'
-import {uploadFile} from './uploadFileUtil.js'
-import {CHANGE_RINGTONE,SEND_PREFERENCE,LANGUAGE,SELECT_LANGUAGES,CHANGE_NOTIFICATION_TIME} from './constantSetting.js'
 import Select from '../../stories/Select/Select.jsx'
+import {uploadFile} from './uploadFileUtil.js'
+import {LANGUAGE,LABELS} from './constantSetting.js'
 const Settings = ({currentUser={}}) => {
     const {emailFrequency,sendNotificationTime,_id,soundVoice}= currentUser.preferences
     const url=process.env.REACT_APP_BASE_URL;
     const preferencesUrl=`${url}/preferences/${_id}`
     const [ringtoneFile, setRingtoneFile] = useState(null);
-    const [audioSrc,setAudioSrc]  = useState();
+    const [audioSrc,setAudioSrc]  = useState(`${url}/uploads/${soundVoice}`);
     const [notificationTime,setNotificationTime]= useState(sendNotificationTime);
     const [lng,setLng] =useState('en');
     const {t,i18n}= useTranslation();
@@ -43,19 +43,19 @@ const Settings = ({currentUser={}}) => {
     
     return (
       <> 
-      <div> 
+        <div> 
           <div className='uploadWarper'>
 
-            <GenericInput  type='file'  label={t(CHANGE_RINGTONE)} onChange={handleFileChange} size='medium'  />
+            <GenericInput  type='file'  label={t(LABELS.CHANGE_RINGTONE)} onChange={handleFileChange} size='medium'  />
           </div>
           <div>
-            {audioSrc && <audio controls>
+             <audio controls>
                <source src={audioSrc} ></source>
-            </audio>}
+            </audio>
             
           </div>       
 
-         <Select  title={t(SELECT_LANGUAGES)} 
+         <Select  title={t(LABELS.SELECT_LANGUAGES)} 
           options={[{text:LANGUAGE.he,value:'he'}, {text:LANGUAGE.es,value:'es'},{text:LANGUAGE.en,value:'en',}]} 
           className='' 
           size={'large'}
@@ -63,13 +63,8 @@ const Settings = ({currentUser={}}) => {
           value={lng}
           onChange={e=>handleLngChange(e.target.value)}/>
         </div>          
-        
-         <GenericButton size='small'  label={t(SEND_PREFERENCE)} onClick={sendPreference} className='' disabled={false}/>
-
-          <GenericInput size='small'  label={t(CHANGE_NOTIFICATION_TIME)} onChange={changeNotificationTime} type='number' className='' disabled={false}/>
-          <GenericButton size='small'  label={t(SEND_PREFERENCE)} onClick={sendPreference} className='' disabled={false}/>
-          
-
+          <GenericInput size='small'  label={t(LABELS.CHANGE_NOTIFICATION_TIME)} onChange={changeNotificationTime} type='number' className='' />
+          <GenericButton size='small'  label={t(LABELS.SEND_PREFERENCE)} onClick={sendPreference} className='but-send'/>
       </>
          
     );
--- client-side/src/components/settings/constantSetting.js
+++ client-side/src/components/settings/constantSetting.js
@@ -1,11 +1,14 @@
- const SEND_PREFERENCE='send preferences';
- const CHANGE_RINGTONE='change-ringtone';
- const SELECT_LANGUAGES='sleect-language'
- const CHANGE_NOTIFICATION_TIME='change-notification-time'
+ 
+ const LABELS={
+    SEND_PREFERENCE:'send preferences',
+    CHANGE_RINGTONE:'change-ringtone',
+    SELECT_LANGUAGES:'sleect-language',
+    CHANGE_NOTIFICATION_TIME:'change-notification-time'
+ };
  const LANGUAGE ={
     he:'עברית',
     en:'english',
     es:'española'
-}
-export{SEND_PREFERENCE,CHANGE_RINGTONE,SELECT_LANGUAGES,LANGUAGE,CHANGE_NOTIFICATION_TIME}
+};
+export{LANGUAGE,LABELS}
 
