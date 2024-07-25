74b5c4f65576dbb95b817b0e0d53daafe0c8d62b
Shira-Abrams <sh3000444@gmail.com>
2024-07-25 13:40:49 +0300

    fix again

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -8,21 +8,10 @@ import './App.scss';
 
 function App() {
 
-  //const {emailFrequency,sendNotificationTime,_id,soundVoice}= currentUser.preferences
-
- const user= {
-  _id:'fdfdfd',
-  "preferences": {
-    "_id": "66953d2791606a13857abd26",
-    "emailFrequency": "weekly",
-    "sendNotificationTime": 56,
-    "soundVoice": "× ×\u0095×ª×\u0099 ×\u009c×\u0099×\u0091×¨×\u009e×\u009f - ×\u0099×\u009d ×©×\u009c ×\u0093×\u009e×¢×\u0095×ª.mp3",
-    "__v": 0
-  }} 
+  
   return (
     <div className="App">
       <Header/>
-       <Setting currentUser={user}/>
       <Footer/>
     </div>
   );
