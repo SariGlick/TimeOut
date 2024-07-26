25618817de304d846dc8773ab68fa99f184fa957
Malky Sinowitz <malkysino@gmail.com>
2024-07-26 17:10:43 +0300

    did change

--- server-side/email-service/app/mailer.js
+++ server-side/email-service/app/mailer.js
@@ -9,9 +9,6 @@ let transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
 
