2236da98a383bfe79fdb9fcdc79ace6156c892c2
Batsheva <batshevatavger@gmail.com>
2024-07-28 13:19:31 +0300

    fix changes

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ThemeProvider from './stories/themes/ThemeProvider';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 reportWebVitals();
