ae7f290ba99d90888abd99726e5d05646832945d
Shira-Abrams <sh3000444@gmail.com>
2024-07-29 14:17:21 +0300

    fix for mergi

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,9 +1,9 @@
 
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import Footer from './stories/footer/FooterComponent';
 import { router } from './router/router.jsx';
 import { store } from './redux/store.jsx';
+import Footer from './stories/footer/FooterComponent';
 import './App.scss';
 function App() {
   
