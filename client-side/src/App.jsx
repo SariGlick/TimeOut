f82fd1e93c9d9f0970cbe5c7e65f1087a1238555
ayeletBinder <ayeletbinder@gmail.com>
2024-07-31 15:15:11 +0300

    remove from app.js

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -4,14 +4,12 @@ import Footer from './stories/footer/FooterComponent';
 import { router } from './router/router.jsx';
 import { store } from './redux/store.jsx';
 import './App.scss';
-import BasicPopup from './stories/popup/popup.jsx';
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
       <Provider store={store}>
-        <BasicPopup />
         <Footer />
       </Provider>
     </>
