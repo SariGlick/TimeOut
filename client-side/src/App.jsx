caafbdb0f37dccdf17edc43ff2caf36d95aa56ea
ayeletBinder <ayeletbinder@gmail.com>
2024-07-31 15:09:51 +0300

    without button

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -4,12 +4,14 @@ import Footer from './stories/footer/FooterComponent';
 import { router } from './router/router.jsx';
 import { store } from './redux/store.jsx';
 import './App.scss';
+import BasicPopup from './stories/popup/popup.jsx';
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
       <Provider store={store}>
+        <BasicPopup />
         <Footer />
       </Provider>
     </>
--- client-side/src/stories/popup/popup.jsx
+++ client-side/src/stories/popup/popup.jsx
@@ -3,19 +3,11 @@ import { Unstable_Popup as BasePopup } from '@mui/base/Unstable_Popup';
 import GenericButton from '../Button/GenericButton';
 import './popup.scss';
 
-export default function BasicPopup({labelButtonOutThePopup='open popup',popupContent }) {
-  const [anchor, setAnchor] = React.useState(null);
-  const handleClick = (event) => {
-    setAnchor(anchor ? null : event.currentTarget);
-  };
-
-  const open = Boolean(anchor);
-  const id = open ? 'simple-popup' : undefined;
+export default function BasicPopup({popupContent }) {
 
   return (
     <div>
-      <GenericButton className="secondary" label={labelButtonOutThePopup} aria-describedby={id} type="button" onClick={handleClick} />
-      <BasePopup id={id} open={open} anchor={anchor}>
+      <BasePopup>
         <div className='popup-body'>
            {popupContent}
         </div>
