588b8dcfabf165ecbe420ebee03c15d42f30e834
bati finkelshtein <b0556729929@gmail.com>
2024-08-01 14:07:51 +0300

    update the content file

--- extension-ui/content.js
+++ extension-ui/content.js
@@ -1,18 +1,29 @@
 let blockedSitesCache = null;
+let allowedSitesCache = null; // משתנה חדש לרשימה הלבנה
 
-function initializeBlockedSitesCache(callback) {
-  chrome.storage.local.get("blockedSites", (data) => {
+function initializeCache(callback) {
+  chrome.storage.local.get(["blockedSites", "allowedSites"], (data) => {
     blockedSitesCache = data.blockedSites || [];
+    allowedSitesCache = data.allowedSites || []; // טען את הרשימה הלבנה
     if (callback) callback();
   });
 }
 
-initializeBlockedSitesCache(() => {
+// שינוי פונקציה לinitializeCache
+initializeCache(() => {
   const hostname = window.location.hostname.toLowerCase();
   const domain = hostname.split(".")[1];
-  if (blockedSitesCache && blockedSitesCache.includes(domain)) {
-    window.location.href = chrome.runtime.getURL('oops.html');
+
+  // בדיקה לפי משתנה isBlackList
+  if (isBlackList) {
+    // חסום אם האתר נמצא ברשימת החסומות
+    if (blockedSitesCache && blockedSitesCache.includes(domain)) {
+      window.location.href = chrome.runtime.getURL('oops.html');
+    }
+  } else {
+    // חסום אם האתר אינו נמצא ברשימת האתרים המורשים
+    if (allowedSitesCache && !allowedSitesCache.includes(domain)) {
+      window.location.href = chrome.runtime.getURL('oops.html');
+    }
   }
 });
-
-
