ca2ce05a5cd04959b7bd111bd00a14356f2716dd
dvora <dvoras2000@gmail.com>
2024-08-02 00:20:02 +0300

    the function is ready

--- server-side/profileMngr.js
+++ server-side/profileMngr.js
@@ -1,13 +1,16 @@
 import axios from 'axios'
 
 export default function activeProfile(userId) {
+    const hourNow = new Date().toLocaleTimeString('en-US', { hour12: false }).split(':')[0];
     const user = axios.get(`http://localhost:3000/users/:${userId}`)
     user.then((user) => {
-        const fieldValue = user.data.profiles.map((item) => item.listWebsites.map((website) => website.limitedTimes.map((item) =>
-            //new Date().getHours() >= 
-        new Date(item.start).getHours()// && new Date().getHours() <=new Date(item.end).getHours()?item:null
-
-        )))//?.map((website) => website.listWebsites))
+        const fieldValue = user.data.profiles.map((item) => item.listWebsites.filter((website) => {
+            return website.limitedTimes.some((item) => {
+                return item.start.split('T')[1].split('.')[0].split(':')[0] <= hourNow &&
+                    item.end.split('T')[1].split('.')[0].split(':')[0] >= hourNow
+            })
+        }
+        ))
         console.log(fieldValue);
     });
     return user;
