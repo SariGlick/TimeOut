84fe71560381c8e0f9c09d8f01a61ca2e1dd806e
lielsa2023 <lielsa1160@gmail.com>
2024-08-01 14:00:00 +0300

    delete comments

--- client-side/src/services/limitTheMessage.js
+++ client-side/src/services/limitTheMessage.js
@@ -1,14 +1,10 @@
 
-//TODO:לבדוק האם נכון לעשות את זה כאן,  בסרוויס נפרד
-//או שנכון יותר בקונטרולר עצמו, כמו כן האם כדאי להוסיף לקונטרולר
-//של ההעדפות קבלת ההעדפות לפי מזהה משתמש
-//או להשאיר את זה פה
+
 import axiosInstance from '../axios/axios.js';
 
 const getPreferenceByUserId = async (userId) => {
     try {
-        const response = await axiosInstance.get(`/users/${userId}`);
-        console.log(response);
+        const response = await axiosInstance.get(`/users/${userId}`);       
         const countOfMessagees = response.data.preference.countOfMessagees;
         return countOfMessagees;         
          
@@ -21,8 +17,7 @@ export  const getLimitMessages = async (id) => {
     try {
         const countOfMessagees = await getPreferenceByUserId(id);
         const response = await axiosInstance.get(`/message/user/${id}`);
-        const Messages = response.data; 
-        console.log(Messages);
+        const Messages = response.data;        
         
         if (Messages.length > countOfMessagees) {
             return Messages.slice(-countOfMessagees); 
@@ -36,14 +31,3 @@ export  const getLimitMessages = async (id) => {
 };
 
 
-//  export const limitTheMessage = async (userId) => {
-//     const countOfMessagees = await getPreferenceByUserId(userId);
-//     const messages = await getMessagesByUserId(userId);
-//     Messages.push(...messages);
-//     while (Messages.length > countOfMessagees) {
-//         Messages.shift();
-//     }
-//     return Messages;
-//  }   
-
-// export default limitTheMessage
