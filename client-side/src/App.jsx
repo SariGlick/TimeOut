2bc543f063c653e2fa0f79df57898c68a4c1e85a
Shira-Abrams <sh3000444@gmail.com>
2024-08-01 17:13:14 +0300

    account tab

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -5,7 +5,9 @@ import { router } from './router/router.jsx';
 import { store } from './redux/store.jsx';
 import './App.scss';
 import Header from './stories/header/header'
+import VerticalTabs from './stories/verticalTabs/verticalTabss.jsx'
 import Footer from './stories/footer/FooterComponent'
+import AccountTab from './components/settings/accountTab.jsx'
 function App() {
  
   
@@ -13,6 +15,7 @@ function App() {
 
     <>
       <Provider store={store}>
+        <VerticalTabs labels={['Account']} elements={[<AccountTab/>]}/>
         <Footer />
       </Provider>
     </>
