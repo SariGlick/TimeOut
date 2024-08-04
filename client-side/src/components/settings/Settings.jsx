bb4f2a1d8c064c3f18a562278857f5921012bfca
moriya2004 <145592854+moriya2004@users.noreply.github.com>
2024-08-04 23:43:58 +0300

    Update Settings.jsx

--- client-side/src/components/settings/Settings.jsx
+++ client-side/src/components/settings/Settings.jsx
@@ -5,28 +5,11 @@ import Preferences from './Preferences.jsx';
 import AccountTab from './accountTab';
 import Notifications from './Notifications.jsx';
 import {LABELS} from './constantSetting.js'
-const user={
-  "_id": "66940b051ccb2852370d5a17",
-  "name": "Alice Johnson",
-  "email": "alice.johnson@example.com",
-  "password": "securePassword123",
-  "visitsWebsites": [],
-  "profiles": [],
-  "profileImage": "profile.jpg",
-  "preference": {
-      "sendNotificationTime": 30,
-      "soundVoice": "×××ª ××¢× ×¢×©×",
-      "_id": "66930c2e2aad987e24078e12",
-      "emailFrequency": "weekly",
-      "timeZone": "UTC",
-      "language": "he"
-  }
-}
 
 const  Settings =()=> {
   const elements = [
     <AccountTab/>,
-    <Notifications currentUser={user}/>,
+    <Notifications/>,
     <Preferences/>
   ] 
   const {t} = useTranslation();
