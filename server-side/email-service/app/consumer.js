4124d2c3c0f96d69aa0b160cfebfc2822db1eb71
Malky Sinowitz <malkysino@gmail.com>
2024-08-05 15:03:11 +0300

    n

--- server-side/email-service/app/consumer.js
+++ server-side/email-service/app/consumer.js
@@ -1,8 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
+
 import kafka from 'kafka-node';
 import { sendEmail } from './mailer.js';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const Consumer = kafka.Consumer;
 const client = new kafka.KafkaClient({ kafkaHost: process.env.KAFKA_BROKER });
 const consumer = new Consumer(client, [{ topic: 'emailTopic', partition: 0 }], { autoCommit: true });
