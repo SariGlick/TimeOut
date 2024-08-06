85e022eef67aa401bc0a499c8da5e3e778e7b68f
Shira-Abrams <sh3000444@gmail.com>
2024-08-06 09:59:07 +0300

    delete select

--- client-side/src/components/settings/constantSetting.js
+++
@@ -1,5 +0,0 @@
- const SEND_PREFERENCE='send preferences';
- const CHANGE_RINGTONE='change-ringtone';
- const SELECT_LANGUAGES='sleect-language'
-export {SEND_PREFERENCE,CHANGE_RINGTONE,SELECT_LANGUAGES}
-
