79148b097b9a2e3cbf4cafb8347583a44a13b2ac
Shira-Abrams <sh3000444@gmail.com>
2024-08-06 12:45:33 +0300

    add all preference

--- server-side/tests/preference.test.js
+++ server-side/tests/preference.test.js
@@ -1,2 +1,15 @@
 import request from 'supertest';
+import app from '../app.js';
 import { connectMongo, disconnectMongo } from '../config/db.js'
+beforeAll(async () => {
+    await connectMongo(); // Connects to a database before the tests
+  });
+  
+  afterAll(async () => {
+    await disconnectMongo(); // Closes database connection after tests
+  });
+
+  describe('get all preference /' ,()=>{
+      it('should return all preference')
+    
+  })
