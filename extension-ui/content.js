0794120ea332b49934d03c5bd8821695d46ea1f7
Malky Sinowitz <malkysino@gmail.com>
2024-08-06 11:14:24 +0300

    copied the original content from main and replaced

--- extension-ui/content.js
+++ extension-ui/content.js
@@ -1,29 +1,18 @@
 let blockedSitesCache = null;
-let allowedSitesCache = null; // משתנה חדש לרשימה הלבנה
 
-function initializeCache(callback) {
-  chrome.storage.local.get(["blockedSites", "allowedSites"], (data) => {
+function initializeBlockedSitesCache(callback) {
+  chrome.storage.local.get("blockedSites", (data) => {
     blockedSitesCache = data.blockedSites || [];
-    allowedSitesCache = data.allowedSites || []; // טען את הרשימה הלבנה
     if (callback) callback();
   });
 }
 
-// שינוי פונקציה לinitializeCache
-initializeCache(() => {
+initializeBlockedSitesCache(() => {
   const hostname = window.location.hostname.toLowerCase();
   const domain = hostname.split(".")[1];
-
-  // בדיקה לפי משתנה isBlackList
-  if (isBlackList) {
-    // חסום אם האתר נמצא ברשימת החסומות
-    if (blockedSitesCache && blockedSitesCache.includes(domain)) {
-      window.location.href = chrome.runtime.getURL('oops.html');
-    }
-  } else {
-    // חסום אם האתר אינו נמצא ברשימת האתרים המורשים
-    if (allowedSitesCache && !allowedSitesCache.includes(domain)) {
-      window.location.href = chrome.runtime.getURL('oops.html');
-    }
+  if (blockedSitesCache && blockedSitesCache.includes(domain)) {
+    window.location.href = chrome.runtime.getURL('oops.html');
   }
 });
+
+
