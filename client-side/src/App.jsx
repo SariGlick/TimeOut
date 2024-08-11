884975685d724e8d11da81d26827d853c877d3a0
Shira-Abrams <sh3000444@gmail.com>
2024-08-11 19:46:35 +0300

    fix coverstaion

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -3,15 +3,13 @@ import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router.jsx';
 import { store } from './redux/store.jsx';
+import { SnackbarProvider } from 'notistack'
 import Footer from './stories/footer/FooterComponent';
-import { SnackbarProvider } from 'notistack';
 import './App.scss';
 function App() {
   
   
   return (
-
-
     <> 
       <SnackbarProvider maxSnack={3}>
       <Provider store={store}>
