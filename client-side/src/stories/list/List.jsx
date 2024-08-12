16cf827232454015a75a2d2936f7f31ade358379
ruth4453 <4453ruth@gmail.com>
2024-08-12 12:04:46 +0300

    change to genery componenet

--- client-side/src/stories/list/List.jsx
+++ client-side/src/stories/list/List.jsx
@@ -1,125 +1,51 @@
 import React from 'react';
-import { useState } from 'react';
-import axios from 'axios';
 import PropTypes from 'prop-types';
-import {
-    List,
-    ListItem,
-    ListItemText,
-    IconButton,
-} from '@mui/material';
-import CommentIcon from '@mui/icons-material/Comment';
-import { Button, Typography, Tooltip } from '@mui/material';
-import { Visibility, Delete, DataObject } from '@mui/icons-material';
-import './List.scss';
+import { List } from '@mui/material';
+import './list.scss'
 
-
-const ListComponent = ({dataObject}) => {
-    
-
-    const [hoveredItem, setHoveredItem] = useState(null);
-    const [isHovered, setIsHovered] = useState(false);
-
-    const handleMouseEnter = (itemId) => {
-        setHoveredItem(itemId);
-        setIsHovered(true);
-
-    };
-
-    const handleMouseLeave = () => {
-        setHoveredItem(null);
-        setIsHovered(false);
-
-
-    };
-
-    const handleToggleRead =async (itemId,read,type) => {
-       const newMessage={
-        _id:itemId,
-        read:!read,
-        type:type
-       }
-       try {
-        await axios.put(`http://localhost:5000/message/${itemId}`,newMessage)
-          .then(res => {
-            console.log(res.data)
-          })
-      }
-      catch {
-        console.log("faild")
-      }
-          
-        };
-
-    const deleteMassege = async(itemId) => {
-        try {
-            await axios.delete(`http://localhost:5000/message/${itemId}`)
-              .then(res => {
-                console.log(res.data)
-              })
-          }
-          catch {
-            console.log("faild")
-          }
-    };
-
+const GenericList = ({ messages, onHover, onMouseOut, iconsArr=null, hoveredIndex }) => {
     return (
-        <div className="wrapper">
+        <div className='wrapper'>
             <List>
-                {dataObject.messageData.map(({ _id, read, type }) => (
-                    <ListItem
-                        key={_id}
-                        onMouseEnter={() => handleMouseEnter(_id)}
-                        onMouseLeave={handleMouseLeave}
-                        disableGutters
-                        secondaryAction={
-                            <IconButton aria-label="comment">
-                                <CommentIcon />
-                            </IconButton>
-                        }
-                    >
-                        <ListItemText primary={type.type} className={read ? 'read' : 'unread'} />
-                        {hoveredItem === _id && (
-                            <Tooltip title={read ? 'Mark as unread' : 'Mark as read'}>
-                                <div className="icons-wrapper">
-
-                                    <IconButton
-                                        className="eye-icon"
-                                        onClick={() => handleToggleRead(_id,read,type)}
-                                        style={{ color: read ? '#474747' : 'rgb(27, 27, 27)' }}
-                                    >
-
-                                        {read && <div className="line"></div>}
-                                        <Visibility />
-                                    </IconButton>
-                                    <IconButton onClick={() => deleteMassege(_id)}>
-                                        <Delete />
-
-                                    </IconButton>
+                {messages && messages.length > 0 ? (
+                    messages.map((MessageComponent, index) => (
+                        <div 
+                            key={index} 
+                            onMouseOver={() => onHover(index)}
+                            onMouseOut={onMouseOut}
+                            style={{ position: 'relative' }}
+                        >
+                            {React.isValidElement(MessageComponent) ? (
+                                <div>
+                                    {MessageComponent}
+                                    {hoveredIndex === index && (
+                                        <div className="icons-container">
+                                            {iconsArr[index]}
+                                        </div>
+                                    )}
                                 </div>
-
-                            </Tooltip>
-                        )}
-                    </ListItem>
-                ))}
+                            ) : (
+                                <div>Invalid component</div>
+                            )}
+                        </div>
+                    ))
+                ) : (
+                    <p>No messages found.</p>
+                )}
             </List>
         </div>
     );
 };
 
-ListComponent.propTypes = {
-    dataObject: PropTypes.shape({
-        messageData: PropTypes.arrayOf(
-            PropTypes.shape({
-                _id: PropTypes.string.isRequired,
-                read: PropTypes.bool.isRequired,
-                type: PropTypes.shape({
-                    type: PropTypes.string.isRequired,
-                }).isRequired,
-            })
-        ).isRequired,
-    }).isRequired,
+
+
+GenericList.propTypes = {
+    messages: PropTypes.arrayOf(PropTypes.element).isRequired,
+    onHover: PropTypes.func.isRequired,
+    onMouseOut: PropTypes.func.isRequired,
+    iconsArr: PropTypes.arrayOf(PropTypes.element).isRequired,
+    hoveredIndex: PropTypes.number.isRequired,
 };
 
-export default ListComponent;
 
+export default GenericList;
