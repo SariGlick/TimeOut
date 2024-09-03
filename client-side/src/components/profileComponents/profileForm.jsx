d4558f01388d4d5d87bb164a8acc470b9d6cf638
MiryamGabay <miryamgabay9@gmail.com>
2024-09-03 15:09:32 +0300

    Adding the timer changes

--- client-side/src/components/profileComponents/profileForm.jsx
+++ client-side/src/components/profileComponents/profileForm.jsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import { Grid, Box, Tooltip, Checkbox, FormControlLabel } from '@mui/material';
+import { Grid, Box, Tooltip } from '@mui/material';
 import GenericInput from '../../stories/GenericInput/genericInput.jsx';
 import Select from '../../stories/Select/Select.jsx';
-import MapComponent from '../googleServices/googleMap.jsx';
+import { INPUT_LABELS, TOOLTIP_TEXTS, SELECT_OPTIONS } from '../../constants/profileConstants.js';
 import { handleFieldChange } from '../../utils/profileUtil.js';
-import {
-    INPUT_LABELS,
-    TOOLTIP_TEXTS,
-    SELECT_OPTIONS
-} from '../../constants/profileConstants.js';
-import '../../styles/profilePageStyle.scss';
 
 export default function ProfileForm({ formData, setFormData }) {
-
-    const handleSaveDataAddress = ({ address, markerPosition }) => {
-        debugger
-        setFormData(prevData => ({
-            ...prevData,
-            googleMapsLocation: {
-                address: address,
-                lat: markerPosition.lat,
-                lng: markerPosition.lng
-            }
-        }));
-    };
-
     return (
         <Box mt={2}>
             <Grid container spacing={3}>
@@ -81,60 +62,6 @@ export default function ProfileForm({ formData, setFormData }) {
                                 onChange={(e) => handleFieldChange(e, setFormData)}
                                 options={SELECT_OPTIONS.STATUS_BLOCKED_SITES}
                                 widthOfSelect='100%'
-                            />
-                        </Tooltip>
-                    </Box>
-                </Grid>
-            </Grid>
-            <Grid container spacing={3} mt={2}>
-                <Grid item xs={12} sm={4}>
-                    <Box mt={1}>
-                        <Tooltip title={TOOLTIP_TEXTS.GOOGLE_MAP}>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        name="googleMapsEnabled"
-                                        checked={formData.googleMapsEnabled}
-                                        onChange={(e) => handleFieldChange(e, setFormData)}
-                                        className="custom-checkbox"
-                                    />
-                                }
-                                label={INPUT_LABELS.GOOGLE_MAP}
-                            />
-                        </Tooltip>
-                        {formData.googleMapsEnabled && <MapComponent onSaveData={handleSaveDataAddress}/>}
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Box mt={1}>
-                        <Tooltip title={TOOLTIP_TEXTS.GOOGLE_CALENDAR}>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        name="googleCalendarEnabled"
-                                        checked={formData.googleCalendarEnabled}
-                                        onChange={(e) => handleFieldChange(e, setFormData)}
-                                        className="custom-checkbox"
-                                    />
-                                }
-                                label={INPUT_LABELS.GOOGLE_CALENDAR}
-                            />
-                        </Tooltip>
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Box mt={1}>
-                        <Tooltip title={TOOLTIP_TEXTS.GOOGLE_DRIVE}>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        name="googleDriveEnabled"
-                                        checked={formData.googleDriveEnabled}
-                                        onChange={(e) => handleFieldChange(e, setFormData)}
-                                        className="custom-checkbox"
-                                    />
-                                }
-                                label={INPUT_LABELS.GOOGLE_DRIVE}
                             />
                         </Tooltip>
                     </Box>
